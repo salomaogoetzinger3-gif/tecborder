6082c01e173ec364693913c11206f082b32207ee
SalomAo-Goetzinger <salomaogoetzinger3@gmail.com>
2026-04-16 12:15:38 -0300

    Aula3 finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,80 @@
 import "./App.css";
+import { Banner } from "./componentes/Banner";
+import { CardEvento } from "./componentes/CardEvento";
 import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 // no react, componentes são FUNÇÕES
 // props é um OBJETO
 // props.children
 
 function App() {
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end",
+    },
+    {
+      id: 2,
+      nome: "back-end",
+    },
+    {
+      id: 3,
+      nome: "devops",
+    },
+    {
+      id: 4,
+      nome: "inteligencia artificial",
+    },
+    {
+      id: 5,
+      nome: "data Science",
+    },
+    {
+      id: 6,
+      nome: "cloud",
+    },
+  ];
+
+  const eventos = [
+    {
+      capa: "http://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png",
+      tema: temas[0],
+      data: new Date(),
+      titulo: "Mulheres no Front",
+    },
+  ];
+
   return (
     <main>
       <header>
         <img src="/Logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+            <CardEvento evento={eventos[0]} />
+          </section>
+        );
+      })}
+      {/*       <section>
+        <Tema tema={temas[1]} />
+      </section>
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+      <section>
+        <Tema tema={temas[5]} />
+      </section> */}
     </main>
   );
 }
